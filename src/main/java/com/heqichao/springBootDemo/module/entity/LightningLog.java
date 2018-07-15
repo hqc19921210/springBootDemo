5b0c938c285de49f4ed9eab90c947d826a0ca77c
hqc19921210 <513776672@qq.com>
2018-07-15 15:04:49 +0800

    '补充module'

---
+++ src/main/java/com/heqichao/springBootDemo/module/entity/LightningLog.java
@@ -0,0 +1,13 @@
+package com.heqichao.springBootDemo.module.entity;
+
+import com.heqichao.springBootDemo.base.entity.BaseEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ * 雷击记录
+ * Created by heqichao on 2018-7-15.
+ */
+@Component("lightning_log")
+public class LightningLog extends BaseEntity {
+
+}
---
+++ src/main/java/com/heqichao/springBootDemo/module/service/LightningLogServiceImpl.java
@@ -0,0 +1,10 @@
+package com.heqichao.springBootDemo.module.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by heqichao on 2018-7-15.
+ */
+@Service
+public class LightningLogServiceImpl implements LightningLogService {
+}
